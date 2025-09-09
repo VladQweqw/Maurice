c4b9945d0c9ff2e14246b0daac8b34b8ed18918a
Vlad <vladpoienariU@gmail.com>
2025-09-09 15:32:53 +0300

    scanning the newtork added multiple helper functions

--- helpers.py
+++ helpers.py
@@ -15,7 +15,44 @@ def find_interfaces():
     return addresses
 
 
-def capture_live_packets(isScanning):
+def convert_IP_to_binary(ip):
+    new_ip = ""
+    
+    for octet in ip.split("."):
+        octet = int(octet)
+        n = 7
+        binary_octet = ""
+
+        while(octet >= 0 and n >= 0):
+            power = 2 ** n
+
+            if octet - power >= 0:
+                octet -= power
+                binary_octet += "1"
+            else:
+                binary_octet += "0"
+
+            n -= 1
+        
+        new_ip += binary_octet
+
+    return new_ip
+
+def get_network_ip(host_ip, subnet_mask):
+    host_ip_bin = convert_IP_to_binary(host_ip)
+    subnet_bin = convert_IP_to_binary(subnet_mask)
+
+    network_ip = ""
+    for idx in range(32):
+        if (host_ip_bin[idx] == subnet_bin[idx]) and host_ip_bin[idx] == "1":
+            network_ip += "1"
+        else:
+            network_ip += "0"
+            
+    return network_ip
+
+
+def capture_live_packets(isScanning, user_ip, network_ip, subnet_bits):
     # createa a loop for asyncio thread
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
@@ -28,6 +65,26 @@ def capture_live_packets(isScanning):
         else:
             try:
                 if "IP" in raw_packet:
-                    print(f"{raw_packet.ip.src} -> {raw_packet.ip.dst}")
+                    ip_src = raw_packet.ip.src
+                    ip_dest = raw_packet.ip.dst
+                    
+                    if is_IP_in_network(ip_src, network_ip, subnet_bits):
+                        if ip_src == user_ip:
+                            print(f"Your PC -> {ip_dest}")
+                        else:
+                            print(f"{ip_src} -> {ip_dest}")
+        
             except Exception as e:
                 print(f"Error {e}")
+
+
+
+
+
+def is_IP_in_network(host_ip, network_ip, subnet_bits):
+    host_ip_bin = convert_IP_to_binary(host_ip)
+    
+    for bit in range(subnet_bits.count("1")):
+        if host_ip_bin[bit] != network_ip[bit]: return False
+
+    return True
--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import psutil
 import threading
 
 # get interfaces
-from helpers import find_interfaces, capture_live_packets
+from helpers import find_interfaces, capture_live_packets, get_network_ip, convert_IP_to_binary
 
 root = Tk()
 root.geometry('300x500')
 title = Label(root, text="Network spy", font=("Roboto", 16)).pack(pady=10)
 
 
-current_lan_ip = StringVar(value="LAN IP:")
-current_lan_network = StringVar(value="LAN Network:")
+current_lan_ip = StringVar(value="0.0.0.0")
+current_lan_network = StringVar(value="0.0.0.0")
 
 user_ip_label = Label(root, textvariable=current_lan_ip, font=("Roboto", 12)).pack(pady=2)
 network_ip_label = Label(root, textvariable=current_lan_network, font=("Roboto", 12)).pack(pady=2)
@@ -34,11 +34,10 @@ def on_interface_change(event):
     selected_interface = addresses_interfaces.index(selected_option.get())
     print(selected_interface)
 
-    current_lan_ip.set("LAN IP: " + addresses[selected_interface][1])
-    current_lan_network.set("LAN Network: " + addresses[selected_interface][2])
+    current_lan_ip.set(addresses[selected_interface][1])
+    current_lan_network.set(addresses[selected_interface][2])
 
 dropdown.bind("<<ComboboxSelected>>", on_interface_change)
-
 
 # list
 listbox = Listbox(root, font=("Roboto", 16))
@@ -58,9 +57,12 @@ def start_scanning():
     else:
         btn_text_var.set("Stop SCAN")
         isScanning = True
-        threading.Thread(target=capture_live_packets, args=({
-            'value': isScanning
-        },), daemon=True).start()
+        threading.Thread(target=capture_live_packets, args=(
+            isScanning, 
+            current_lan_ip.get(),
+            get_network_ip(current_lan_ip.get(), current_lan_network.get()), 
+            convert_IP_to_binary(current_lan_network.get())
+        ), daemon=True).start()
     
 
 btn_text_var = StringVar(value="Scan")
